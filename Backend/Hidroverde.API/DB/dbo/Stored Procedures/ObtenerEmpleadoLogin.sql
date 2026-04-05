de9588b866fefa061d28a296f7c90cf01dcff48a
Gerson <gerschacon@gmail.com>
2026-04-05 10:13:36 -0600

    Cambios en los YML

---
+++ Backend/Hidroverde.API/DB/dbo/Stored Procedures/ObtenerEmpleadoLogin.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[ObtenerEmpleadoLogin]
+    @usuario_sistema nvarchar(50),
+    @email           nvarchar(100)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT empleado_id, usuario_sistema, clave_hash, email
+    FROM   [dbo].[Empleados]
+    WHERE  (usuario_sistema = @usuario_sistema)
+       OR  (email           = @email)
+      AND   activo = 1
+END
